d9c4583a89812dc710ee46e8e1c07a7449dfb033
Stancu Florin <niflostancu@gmail.com>
2023-07-01 16:51:48 +0300

    scripts: refactor plugin install (using git now)

--- install_ttrss.sh
+++ install_ttrss.sh
@@ -2,42 +2,75 @@
 # Downloads and installs TinyTinyRSS and plugins
 set -e
 
-DEST=/var/www
+TTRSS_GIT_REPO=https://git.tt-rss.org/fox/tt-rss.git/
+TTRSS_DEST=/var/www
 PLUGINS=(
 	# "https://github.com/dasmurphy/tinytinyrss-fever-plugin/archive/master.tar.gz;plugins;fever"
-	"https://github.com/DigitalDJ/tinytinyrss-fever-plugin/archive/master.tar.gz;plugins;fever:nodir"
-	"https://github.com/levito/tt-rss-feedly-theme/archive/master.tar.gz;themes;feedly"
+	"https://github.com/levito/tt-rss-feedly-theme.git#theme;name=feedly;branch=dist"
+	"https://github.com/DigitalDJ/tinytinyrss-fever-plugin.git#plugin;name=fever"
 )
 
-rm -rf "$DEST"
-git clone https://git.tt-rss.org/fox/tt-rss.git/ "$DEST"
+# workaround for arm/v7 OOM
+echo "Building for $TARGETPLATFORM ..."
+if [[ "$TARGETPLATFORM" == "linux/arm/v7" ]]; then
+	git config --global pack.packSizeLimit 1g
+	git config --global pack.deltaCacheSize 1g
+	git config --global pack.windowMemory 1g
+	git config --global core.packedGitLimit 1g
+	git config --global core.packedGitWindowSize 1g
+fi
 
-for PLUGIN in "${PLUGINS[@]}"; do
-	IFS=';' read -ra _PLUG <<< "$PLUGIN"
-	URL="${_PLUG[0]}"; TYPE="${_PLUG[1]}"; _UNP_FLAGS="${_PLUG[2]}"
-	IFS=":" read -ra _PLUG_FLAGS <<< "${_UNP_FLAGS}"
-	NAME="${_PLUG_FLAGS[0]}"; FLAGS="${_PLUG_FLAGS[1]}"
-	curl -sSL "${URL}" -o "/tmp/${NAME}.tar.gz"
+# Parses an URL fragment and returns each pair on a newline
+# (easy to iterate using `read -r line`)
+# Accepted format: #key1=value;key2=value...
+function parse_url_fragment() {
+	local pair= PAIRS=()
+	if [[ "$1" =~ ^[^#]*#(.+)$ ]]; then
+		IFS=';' read -ra PAIRS <<< "${BASH_REMATCH[1]}"
+		for pair in "${PAIRS[@]}"; do
+			echo "$pair"
+		done
+	fi || true
+}
 
-	#[[ "${_PLUG[1]}" == "themes" ]] && FILES=("*/${_PLUG[2]}*") || true
-	TARARGS=(--strip-components=1)
-	_DEST="/var/www/${TYPE}/"
-	if [[ "$FLAGS" = "nodir" ]]; then
-		# plugin has no self-named dir, create it
-		_DEST="/var/www/${TYPE}/$NAME/"
-	else
-		# only extract the plugin-named subdirectory and prefixed files (e.g.,
-		# for themes: '$NAME.css')
-		TARARGS+=(--wildcards "*/${NAME}*")
+# clone tt-rss
+rm -rf "$TTRSS_DEST"
+git clone "$TTRSS_GIT_REPO" "$TTRSS_DEST"
+
+# install plugins
+for plugin in "${PLUGINS[@]}"; do
+	TYPE=
+	REPO=${plugin%%#*}
+	CLONE_ARGS=()
+	while IFS= read -r line; do
+		case $line in
+			plugin) TYPE=plugins; ;;
+			theme) TYPE=themes; ;;
+			name=*) NAME=${line#*=}; ;;
+			branch=*) CLONE_ARGS+=(-b "${line#*=}"); ;;
+		esac
+	done < <( parse_url_fragment "$plugin" )
+
+	# Download the plugin's archive
+	_DEST="${TTRSS_DEST}/${TYPE}"
+	_CLONE_DEST="$_DEST/${NAME}"
+	if [[ "$TYPE" == "themes" ]]; then
+		# use a separate subdir for cloning themes
+		_CLONE_DEST="${_DEST}/_${NAME}.git"
 	fi
-	echo "Installing ${TYPE} ${NAME}..."
-	mkdir -p "$_DEST"
-	echo "untar /tmp/${NAME}.tar.gz -C $_DEST ${TARARGS[@]}"
-	tar xpf "/tmp/${NAME}.tar.gz" -C "$_DEST" "${TARARGS[@]}"
-	ls -ld "/var/www/${TYPE}/$NAME"*
+	echo "Cloning $NAME to ${_CLONE_DEST}..."
+	mkdir -p "$(dirname "$_CLONE_DEST")"
+	git clone "${CLONE_ARGS[@]}" "$REPO" "$_CLONE_DEST"
+
+	if [[ "$TYPE" == "themes" ]]; then
+		# copy the theme's files (everything prefixed by $NAME) to themes/ dir
+		cp -rf "$_CLONE_DEST/$NAME"* "$_DEST/"
+	fi
+	ls -lh "$_DEST"
+
 done
 
-cd "$DEST"
+cd "$TTRSS_DEST"
 cp -f config.php-dist config.php
 chown nginx:nginx /var/www -R
 
